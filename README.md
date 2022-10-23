b7e1624aa04aed95ac4b8997b5091f9a48ef6ab3
Mohamed Aly <mohamedalyahmed@aol.com>
2022-10-24 01:21:45 +0200

    Add Files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Flutter-Task1-Day2
+
+## Task 1
+
+![Alt text](/assets/img/task1day2.gif "Optional title")
+
---
+++ lib/HomeScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(child:
+         Column(
+           children: [
+            SizedBox(height: 10,),
+            Image.asset("assets/img/welcome.png"),
+            SizedBox(height: 20,),
+             Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text('Hello From Our Store ♥ Enjoy',style: TextStyle(fontSize: 30),)
+              ),
+              SizedBox(height: 100,),
+              Icon(Icons.qr_code,size: 100,color: Colors.white,)
+           ],
+         )),
+      ),
+    );
+  }
+}
